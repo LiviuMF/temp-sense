421523980a90999b10e9c8930ef002dd9dcd1f4a
Liviu Farcas <liviu.farcas@cyber-solutions.com>
2024-11-08 22:52:23 +0200

    Show current time in django admin

--- temp_sense/api/admin.py
+++ temp_sense/api/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from .models import DeviceData, DeviceReading
 
 
+class ReadingsAdmin(admin.ModelAdmin):
+    list_display = ('dev_eui', 'current_time')
+
+
 admin.site.register(DeviceData)
-admin.site.register(DeviceReading)
+admin.site.register(DeviceReading, ReadingsAdmin)
 admin.site.site_header = "LemonGrass Tech"
 admin.site.site_title = "Temperature sensors"
 admin.site.index_title = "Welcome to the device management portal"
