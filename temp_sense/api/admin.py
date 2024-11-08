de827ec9a9bcf0085e1cbf609f2bf045f2acf4cb
Liviu Farcas <liviu.farcas@cyber-solutions.com>
2024-11-08 20:39:12 +0200

    Customize django admin look

--- temp_sense/api/admin.py
+++ temp_sense/api/admin.py
@@ -4,3 +4,6 @@ from .models import DeviceData, DeviceReading
 
 admin.site.register(DeviceData)
 admin.site.register(DeviceReading)
+admin.site.site_header = "LemonGrass Tech"
+admin.site.site_title = "Temperature sensors"
+admin.site.index_title = "Welcome to the device management portal"
