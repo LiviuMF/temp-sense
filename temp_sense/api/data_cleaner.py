6cd16c695d74b7f1631b8b1a885a5c44f28b34bd
Liviu Farcas <liviu.farcas@cyber-solutions.com>
2024-11-21 21:20:27 +0200

    Gardening

--- temp_sense/api/data_cleaner.py
+++ temp_sense/api/data_cleaner.py
@@ -1,17 +1,17 @@
 from datetime import datetime, timedelta
-from .models import DeviceData, DeviceReading
+
+from .models import DeviceData
 
 
 def process_payload(payload: dict) -> dict:
     device_data_instance = DeviceData.objects.get(
-        dev_eui=payload["deviceInfo"]["devEui"])
+        dev_eui=payload["deviceInfo"]["devEui"]
+    )
     cleaned_data: dict = payload["object"]
     cleaned_data.update(
         {
             "dev_eui": device_data_instance,
-            "timestamp": convert_timestamp_to_current_tz(
-                payload["time"]
-            ),
+            "timestamp": convert_timestamp_to_current_tz(payload["time"]),
             "legacy_id": payload["deviceInfo"]["devEui"],
         }
     )
@@ -21,7 +21,7 @@ def process_payload(payload: dict) -> dict:
 def convert_timestamp_to_current_tz(timestamp: str) -> datetime:
     timestamp_cleaned = timestamp.split(".")[0]
     timestamp_as_datetime: datetime = datetime.strptime(
-        timestamp_cleaned, '%Y-%m-%dT%H:%M:%S'
+        timestamp_cleaned, "%Y-%m-%dT%H:%M:%S"
     )
     tz_diff: timedelta = datetime.now() - timestamp_as_datetime
     tz_hour_diff: int = round(tz_diff.total_seconds() / 3600)
