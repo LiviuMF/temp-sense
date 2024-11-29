1799e8db6d0cb8e35cdf04c3db72bbca9b6bb28c
Liviu Farcas <liviu.farcas@cyber-solutions.com>
2024-11-29 13:06:31 +0200

    Send different discord notification based on results

--- temp_sense/api/management/commands/discord_notification.py
+++ temp_sense/api/management/commands/discord_notification.py
@@ -8,18 +8,24 @@ class Command(BaseCommand):
         devices_without_readings = (
             DeviceData.devices_without_readings_in_the_last_hour()
         )
-        full_message = [f'{35 * "="}']
-        for device in devices_without_readings:
-            device_name_length = len(device.__str__())
-            latest_reading = DeviceReading.objects.filter(dev_eui=device).latest(
-                "timestamp"
-            )
-            latest_timestamp = latest_reading.timestamp.strftime("%H:%M")
+        if devices_without_readings:
+            full_message = [f'{35 * "="}']
+            for device in devices_without_readings:
+                latest_timestamp: str = (
+                    DeviceReading.objects.filter(dev_eui=device)
+                    .latest("timestamp")
+                    .timestamp.strftime("%H:%M")
+                )
 
-            full_message.append(
-                f'{device}{(40 - device_name_length) * "."}{latest_timestamp}'
+                device_name_length = len(device.__str__())
+                full_message.append(
+                    f'{device}{(40 - device_name_length) * "."}{latest_timestamp}'
+                )
+            message = "\n".join(full_message)
+            send_message(
+                f'{35 * "="}\n         ❗Devices without readings❗    {message}'
             )
-        message = "\n".join(full_message)
-        send_message(
-            f"{35 * '='}\n Devices without readings in the last hour {message}"
-        )
+        else:
+            send_message(
+                f'{35 * "="}\n        ✅     ~Systems functional~     ✅\n{35 * "="}'
+            )
