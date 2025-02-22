5a2dabd51949d30243d1ec9dc7d5a6bb12cd65f0
LiviuMF <liviu.m.farcas@gmail.com>
2025-02-22 15:02:35 +0200

    Add management commands to start / stop app

--- temp_sense/api/management/commands/kill_process_for_port.py
+++ temp_sense/api/management/commands/kill_process_for_port.py
@@ -1,7 +1,5 @@
-import os
 import logging
 import signal
-import sys
 from subprocess import Popen, PIPE
 
 from django.core.management.base import BaseCommand
@@ -13,12 +11,11 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-PORT = sys.argv[-1]
-
 def kill_process_for_port(port: str) -> None:
     process_bytes = Popen(['ps', 'x'], stdout=PIPE).communicate()
     lines = process_bytes[0].decode('utf-8').split('\n')
     process_to_be_killed = [line for line in lines if port in line][0].split()[0]
+    
     os.kill(
 	int(process_to_be_killed),
         signal.SIGTERM
@@ -26,11 +23,14 @@ def kill_process_for_port(port: str) -> None:
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("port", type=int)
+        
     def handle(self, *args, **options):
         response = requests.get('https://temp-sense.cohe.ro/')
         if response.status_code != 200:
             try:
-                kill_process_for_port(PORT)
+                kill_process_for_port(options['port'])
                 logger.info(f'Port {PORT} killed')
             except:
                 pass
--- temp_sense/api/management/commands/start_temp_sense.py
+++ temp_sense/api/management/commands/start_temp_sense.py
@@ -1,6 +1,5 @@
 import os
 import logging
-import sys
 
 from django.core.management.base import BaseCommand
 
@@ -11,20 +10,20 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-START_FILE_PATH = sys.argv[1]
-
-
 def start_api(file_path: str):
     os.system(f'chmod +x {file_path}')
     os.system(file_path)
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('path')
+        
     def handle(self, *args, **options):
         response = requests.get('https://temp-sense.cohe.ro/')
         if response.status_code != 200:
             try:
-                start_api(START_FILE_PATH)
+                start_api(options['path'])
                 logger.info(f'Started process')
             except:
                 pass
