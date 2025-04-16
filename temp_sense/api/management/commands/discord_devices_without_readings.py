f725463d2d692b1900a82a714f25ec869f55ace7
Liviu Farcas <liviumf@Livius-MacBook-Pro.local>
2025-04-16 18:11:05 +0300

    Round values for better visual

--- temp_sense/api/management/commands/discord_devices_without_readings.py
+++ temp_sense/api/management/commands/discord_devices_without_readings.py
@@ -17,7 +17,7 @@ def get_remaining_tokens_and_days():
 
     total_available_tokens = settings.TOTAL_AVAILABLE_TOKENS - total_tokens_used
 
-    return total_available_tokens, days_left_of_tokens
+    return round(total_available_tokens, 0), round(days_left_of_tokens, 0)
 
 
 class Command(BaseCommand):
