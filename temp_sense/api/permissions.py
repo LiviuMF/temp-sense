b3d74ec12f63bc0a226f7f25c6dfc2cb2cfea15a
liviumf <liviu.m.farcas@gmail.com>
2025-06-15 15:33:02 +0300

    api username change to email

--- temp_sense/api/permissions.py
+++ temp_sense/api/permissions.py
@@ -1,25 +1,24 @@
 from django.conf import settings
 from rest_framework import permissions
 
-from .models import DeviceData
+from .models import DeviceOwner
 
 
 class IsInAllowedGroup(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.username.lower() in settings.API_PERMISSION_EXCEPTIONS:
+        username = request.user.username.lower()
+        if username in settings.API_PERMISSION_EXCEPTIONS:
             return True
 
-        if (request.method == "GET") and (
-            dev_eui := request.query_params.get("dev_eui")
-        ):
+        if request.method == "GET":
             allowed_groups = ["api"]
-            obj = DeviceData.objects.filter(dev_eui=dev_eui).first()
+            obj = DeviceOwner.objects.filter(email__contains=username).first()
             if obj:
                 user_groups = request.user.groups.values_list("name", flat=True)
                 if (
-                    any(group in allowed_groups for group in user_groups)
-                    and view.get_view_name() == "Device Reading List"
-                ) and obj.dev_owner.lower() == request.user.username.lower():
+                        any(group in allowed_groups for group in user_groups)
+                        and view.get_view_name() == "Device Reading List"
+                ) and obj.email.lower() == username:
                     return True
 
         return False
