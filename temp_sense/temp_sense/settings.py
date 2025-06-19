ad50f84386def0bf41583683b4c3ec909f41998e
liviumf <liviu.m.farcas@gmail.com>
2025-06-19 11:21:02 +0300

    Remove and change secret key

--- temp_sense/temp_sense/settings.py
+++ temp_sense/temp_sense/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+xl&29!(=74y-3w%!x2&uxez6=v3_!12_hg!w87*5f6pszfhew"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
